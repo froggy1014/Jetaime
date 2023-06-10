67a011e1b2652a9263d68b6dcf9c9c9b89549ace
froggy1014 <clcl6084@gmail.com>
2023-06-10 19:48:04 +0900

    🚀: implement chatbutton with animation

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -3,7 +3,7 @@
 import ChevronDown from '@public/svg/chevronDown.svg';
 import Logo from '@public/svg/Logo.svg';
 import CategoryButton from '@src/components/common/CategoryButton';
-import { springUp } from '@src/utils/motion';
+import { slideIn, springUp } from '@src/utils/motion';
 import { motion } from 'framer-motion';
 // import Image from 'next/image';
 
@@ -29,8 +29,15 @@ const Main = () => {
 
       <motion.div
         variants={springUp(0.5)}
-        className='absolute bottom-0 flex w-full justify-center'
+        className='absolute bottom-0 flex w-full flex-col items-center justify-center gap-4'
       >
+        <motion.button
+          type='button'
+          className='mx-2 h-10 w-11/12 rounded-full bg-white text-gray-400 shadow-lg'
+          variants={slideIn('right', 'spring', 0.5, 1)}
+        >
+          무엇이든 물어보세요.
+        </motion.button>
         <ChevronDown className='animate-bounce' />
       </motion.div>
     </motion.section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,23 @@ module.exports = {
   ],
   theme: {
     colors: {
+      black: '#000',
+      white: '#fff',
       background: '#FCFAF4',
       primary: '#956A2E',
       secondary: '#F0EDE0',
+      gray: {
+        50: '#F9FAFB',
+        100: '#F3F4F6',
+        200: '#E5E7EB',
+        300: '#D1D5DB',
+        400: '#9CA3AF',
+        500: '#6B7280',
+        600: '#4B5563',
+        700: '#374151',
+        800: '#1F2937',
+        900: '#111827',
+      },
     },
     extend: {},
   },
