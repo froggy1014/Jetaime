b825fcf7068004f8a0f4e51e929596a45e9d0231
froggy1014 <clcl6084@gmail.com>
2023-06-10 19:40:06 +0900

    🚀: add slideIn animation

--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -13,3 +13,25 @@ export const springUp = (delay: number) => ({
     },
   },
 });
+
+export const slideIn = (
+  direction: 'left' | 'up' | 'right' | 'down',
+  type: 'spring' | 'tween',
+  delay: number,
+  duration: number
+) => ({
+  hidden: {
+    x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
+    y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: 'easeOut',
+    },
+  },
+});
